2023dab7d05b03622bf82476ff12911cd5bab6e4
Sx <64361584+NekoMaru76@users.noreply.github.com>
2021-12-06 23:46:11 +0700

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,18 +1,14 @@
-import Lexer from "./src/engines/Lexer/index.ts";
-import Parser from "./src/engines/Parser/index.ts";
-import AST from "./src/engines/utils/AST.ts";
-import Error from "./src/engines/utils/Error.ts";
-import LexerError from "./src/engines/utils/LexerError.ts";
-import ParserError from "./src/engines/utils/ParserError.ts";
-import Position from "./src/engines/utils/Position.ts";
-import Stack from "./src/engines/utils/Stack.ts";
-import Token from "./src/engines/utils/Token.ts";
-import Trace from "./src/engines/utils/Trace.ts";
-import Transformer from "./src/engines/Transformer/index.ts";
-import Compiler from "./src/engines/Compiler/index.ts";
-import Executer from "./src/engines/Executer/index.ts";
-import Interpreter from "./src/engines/Interpreter/index.ts";
-
-export {
-  Lexer, Parser, AST, Error, LexerError, ParserError, Position, Stack, Token, Trace, Transformer, Compiler, Executer, Interpreter
-};
+export { default as Lexer } from "./src/engines/Lexer/index.ts";
+export { default as Parser } from "./src/engines/Parser/index.ts";
+export { default as AST } from "./src/engines/utils/AST.ts";
+export { default as Error } from "./src/engines/utils/Error.ts";
+export { default as LexerError } from "./src/engines/utils/LexerError.ts";
+export { default as ParserError } from "./src/engines/utils/ParserError.ts";
+export { default as Position } from "./src/engines/utils/Position.ts";
+export { default as Stack } from "./src/engines/utils/Stack.ts";
+export { default as Token } from "./src/engines/utils/Token.ts";
+export { default as Trace } from "./src/engines/utils/Trace.ts";
+export { default as Transformer } from "./src/engines/Transformer/index.ts";
+export { default as Compiler } from "./src/engines/Compiler/index.ts";
+export { default as Executer } from "./src/engines/Executer/index.ts";
+export { default as Interpreter } from "./src/engines/Interpreter/index.ts";
