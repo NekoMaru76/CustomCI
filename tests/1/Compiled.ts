133396cff4ff7393974c42e0904f28cbc7c995c7
NekoMaru76 <careday17@gmail.com>
2021-12-07 13:25:56 +0700

    Removed tests/1/Compiled.ts

--- tests/1/Compiled.ts
+++
@@ -1,3 +0,0 @@
-const sum = (...args: number[]) => args.reduce((a: number, b: number) => a+b);
-const sub = (...args: number[]) => args.reduce((a: number, b: number) => a-b);
-console.log(sum(10,1))
